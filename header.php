014321244054b59a3d70d66ed9d58ae7d13e8b34
Lucas Hideki <lucashideki87@gmail.com>
2019-12-21 11:26:37 -0300

    Header now displays "Posts"

--- header.php
+++ header.php
@@ -1,17 +1,23 @@
 <header>
-    <div style="float: left; height: 100%;">
+    <div style="float: left; height: 100%; width: 150px">
         <a class="logopage" href="/web/?page=home">
-        <img class="logo" draggable="false" src="/web/img/blueguy.svg" alt="Logo">
-    </a>
+            <img class="logo" draggable="false" src="/web/img/blueguy.svg" alt="Logo">
+            <?php
+            if (isset($_COOKIE["login"])) {
+                echo '<div style="float: right; padding-top: 2vh">
+          <a class="lead-medium link-header" style="font-size: 4vh" href="/web/posts">Posts</a>
+      </div>';
+            }
+            ?>
+        </a>
     </div>
     <?php
-    if(!isset($_COOKIE["login"])){
-    echo '<div style="float: right; padding-top: 2vh">
+    if (!isset($_COOKIE["login"])) {
+        echo '<div style="float: right; padding-top: 2vh">
         <a class="lead-medium link-header" style="font-size: 4vh" href="/web/?page=login">Login</a>
         <a class="lead-medium link-header" style="font-size: 4vh" href="/web/?page=register">Register</a>
     </div>';
-    }
-    else{
+    } else {
         echo '<div style="float: right; padding-top: 2vh">
         <a class="lead-medium link-header" style="font-size: 4vh" href="/web/?logout=true">Logout</a>
     </div>';
